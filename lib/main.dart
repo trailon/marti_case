805023df605fd8df21b8045447028cf49038d6c1
trailon <cetinelv@gmail.com>
2025-05-26 23:59:20 +0300

    remove some comments && rename template app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
-import 'package:marti_case/template_app.dart';
+import 'package:marti_case/marti_app.dart';
 import 'package:permission_manager/permission_manager.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
 
